339c24aa11dcf77a79dabff29a700e8e59896496
David <youchen.du@gmail.com>
2017-03-21 14:42:39 +0800

    [update] Baked Lightmaps.

---
+++ Assets/Scripts/SelfRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotation : MonoBehaviour {
+	public float rotate_speed = 1f;
+	public Space relative = Space.Self;
+	public Vector3 rotate_direction = Vector3.down;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotate_direction * rotate_speed, relative);
+	}
+}
