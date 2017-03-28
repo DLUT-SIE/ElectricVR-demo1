ded9ae01b037145c62d0ee0f8df4832eb42f69a5
David <youchen.du@gmail.com>
2017-03-28 15:24:11 +0800

    [update] Update explode action with coroutine.

--- Assets/Detonator/System/Detonator.cs
+++ Assets/Detonator/System/Detonator.cs
@@ -282,15 +282,13 @@ public class Detonator : MonoBehaviour {
 	private float _lastExplosionTime = 1000f;
 	void Update () 
     {
-		/*
-		 * if (destroyTime > 0f)
+		if (destroyTime > 0f)
 		{
 			if (_lastExplosionTime + destroyTime <= Time.time)
 			{
 				Destroy(gameObject);
 			}
 		}
-		*/
 	}
 	
 	private bool _firstComponentUpdate = true;
